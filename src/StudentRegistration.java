4be155497d7781459942dc3d77f542baa0e3cd51
Aman Gupta <aman.gupta4692@gmail.com>
2022-09-02 10:10:26 +0530

    Practice Comparable

---
+++ src/StudentRegistration.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class StudentRegistration {
+    public static void main(String[] args) {
+
+        List<Integer> list = Arrays.asList(123422, 543234, 566329, 456312, 943224, 555555, 555556);
+        Collections.sort(list);
+        System.out.println(list);
+    }
+
+
+}
